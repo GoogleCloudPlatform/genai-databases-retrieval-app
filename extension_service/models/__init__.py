17f7fe6a8e19c38128bbdbb1da31751ede4156c4
Yuan Teoh <yuanteoh@google.com>
2023-10-11 21:03:40 +0000

    fix isort

--- extension_service/models/__init__.py
+++ extension_service/models/__init__.py
@@ -12,4 +12,4 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .models import Embedding, Toy, Airport
+from .models import Airport, Embedding, Toy
