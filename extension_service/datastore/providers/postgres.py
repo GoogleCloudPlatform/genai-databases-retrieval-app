98671de439db54e7ac1c2609a171e7f8c04a6b29
Yuan Teoh <yuanteoh@google.com>
2023-10-16 22:43:13 +0000

    add test and mock for postgres airport get endpoint

--- extension_service/datastore/providers/postgres.py
+++ extension_service/datastore/providers/postgres.py
@@ -244,7 +244,7 @@ class Client(datastore.Client[Config]):
         airports = [models.Airport.model_validate(dict(r)) for r in results]
         return airports
 
-    async def get_airport(self, id: str) -> List[Dict[str, Any]]:
+    async def get_airport(self, id: int) -> List[Dict[str, Any]]:
         results = await self.__pool.fetch(
             """
               SELECT iata, name, city, country FROM airports WHERE id=$1
