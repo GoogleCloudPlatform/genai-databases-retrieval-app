04cef91e15c5623492749fb586802be5be315b9b
Yuan Teoh <yuanteoh@google.com>
2023-10-23 14:06:51 -0700

    resolve comments

--- extension_service/datastore/datastore.py
+++ extension_service/datastore/datastore.py
@@ -74,7 +74,7 @@ class Client(ABC, Generic[C]):
         raise NotImplementedError("Subclass should implement this!")
 
     @abstractmethod
-    async def get_airport(self, id: int) -> list[Dict[str, Any]]:
+    async def get_airport(self, id: int) -> list[models.Airport]:
         raise NotImplementedError("Subclass should implement this!")
 
     @abstractmethod
