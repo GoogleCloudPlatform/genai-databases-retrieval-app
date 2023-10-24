1c56f4bee50c6d48fc0556dec778cd0961de48ba
Yuan <45984206+Yuan325@users.noreply.github.com>
2023-10-23 18:39:30 -0700

    feat: update airport dataset and add search endpoint (#14)

--- extension_service/app/app_test.py
+++ extension_service/app/app_test.py
@@ -66,7 +66,7 @@ def test_get_airport(app):
 def test_get_amenity(app):
     with TestClient(app) as client:
         response = client.get(
-            "/amenities",
+            "/airports",
             params={
                 "id": 1,
             },
@@ -77,10 +77,25 @@ def test_get_amenity(app):
     assert output[0]
 
 
-def test_get_airport(app):
+def test_airports_semantic_lookup(app):
     with TestClient(app) as client:
         response = client.get(
-            "/airports",
+            "/airports/semantic_lookup",
+            params={
+                "query": "What is the airport in san francisco.",
+                "top_k": 5,
+            },
+        )
+    assert response.status_code == 200
+    output = response.json()
+    assert len(output) == 5
+    assert output[0]
+
+
+def test_get_amenity(app):
+    with TestClient(app) as client:
+        response = client.get(
+            "/amenities",
             params={
                 "id": 1,
             },
