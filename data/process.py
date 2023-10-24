b39fccb36462d6b1c1b79f68d403428e91831403
Yuan <45984206+Yuan325@users.noreply.github.com>
2023-10-23 18:47:35 -0700

    chore: add unit test for amenities endpoint (#18)

---
+++ data/process.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import random
+import numpy as np
+
+
+
+df = pd.read_csv("/Users/duwenxin/Desktop/SenseAI/database-query-extension/data/flights_dataset.csv", dtype=str)
+
+df = df[(df['departure_airport'] == 'SFO') | (df['arrival_airport'] == 'SFO')]
+# Get the number of rows in the DataFrame
+n_rows = df.shape[0]
+
+
+df.to_csv("/Users/duwenxin/Desktop/SenseAI/database-query-extension/data/flights_dataset.csv", index=False)
+print (n_rows)
