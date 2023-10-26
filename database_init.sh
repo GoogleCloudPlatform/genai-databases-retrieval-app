4ae102cdfd0b61cd157cccfab0cb008990fe2b62
Averi Kitsch <akitsch@google.com>
2023-10-25 17:56:04 -0700

    fix env vars

--- database_init.sh
+++ database_init.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 # Script for Cloud Run Job to initialize AlloyDB data
+export PGPASSWORD=$DB_PASS
+export PGUSER=$DB_USER
 echo "Connecting to $DB_HOST"
 
-createdb -h $DB_HOST -p 5432 -W $DB_PASS $DB_NAME
+createdb -h $DB_HOST -p 5432 $DB_NAME
 
 # List all database
-psql -h $DB_HOST -W $DB_PASS -l
+psql -h $DB_HOST -l
 
 echo "Enabling pgvector"
-psql -h $DB_HOST -d $DB_NAME -W $DB_PASS -c 'CREATE EXTENSION vector;'
+psql -h $DB_HOST -d $DB_NAME -c 'CREATE EXTENSION vector;'
 
 # Change into extension service directory
 cd extension_service
