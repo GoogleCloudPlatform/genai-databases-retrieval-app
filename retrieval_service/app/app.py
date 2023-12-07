85da7c88eb9902d398d93fe27c508ae236a10b0c
Averi Kitsch <akitsch@google.com>
2023-12-07 11:41:18 -0800

    fix: update embeddings and pin model

--- retrieval_service/app/app.py
+++ retrieval_service/app/app.py
@@ -14,16 +14,16 @@
 
 from contextlib import asynccontextmanager
 from ipaddress import IPv4Address, IPv6Address
-from typing import Union
 
+import datastore
 import yaml
 from fastapi import FastAPI
 from langchain.embeddings import VertexAIEmbeddings
 from pydantic import BaseModel
 
-import datastore
-
 from .routes import routes
+
+EMBEDDING_MODEL_NAME = "textembedding-gecko@001"
 
 
 class AppConfig(BaseModel):
@@ -42,7 +42,9 @@ def parse_config(path: str) -> AppConfig:
 def gen_init(cfg: AppConfig):
     async def initialize_datastore(app: FastAPI):
         app.state.datastore = await datastore.create(cfg.datastore)
-        app.state.embed_service = VertexAIEmbeddings()
+        app.state.embed_service = VertexAIEmbeddings(
+            model_name=EMBEDDING_MODEL_NAME
+        )
         yield
         await app.state.datastore.close()
 
---
+++ retrieval_service/run_generate_embeddings.py
@@ -0,0 +1,57 @@
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import asyncio
+import csv
+
+import models
+from app import EMBEDDING_MODEL_NAME
+from langchain.embeddings import VertexAIEmbeddings
+
+
+async def main() -> None:
+    embed_service = VertexAIEmbeddings(model_name=EMBEDDING_MODEL_NAME)
+
+    amenities: list[models.Amenity] = []
+    with open("../data/amenity_dataset.csv", "r") as f:
+        reader = csv.DictReader(f, delimiter=",")
+        for line in reader:
+            amenity = models.Amenity.model_validate(line)
+            amenity.embedding = embed_service.embed_query(amenity.content)
+            amenities.append(amenity)
+
+    print("Completed embedding generation.")
+
+    with open("../data/amenity_dataset.csv.new", "w") as f:
+        col_names = [
+            "id",
+            "name",
+            "description",
+            "location",
+            "terminal",
+            "category",
+            "hour",
+            "content",
+            "embedding",
+        ]
+        writer = csv.DictWriter(f, col_names, delimiter=",")
+        writer.writeheader()
+        for amenity in amenities:
+            writer.writerow(amenity.model_dump())
+
+    print("Wrote data to CSV.")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
