e7c980e06ec97eccd45abbdeada78d2a149e0da5
Yuan Teoh <yuanteoh@google.com>
2024-05-20 14:14:20 -0700

    update langchain library

--- llm_demo/orchestrator/langchain_tools/langchain_tools_orchestrator.py
+++ llm_demo/orchestrator/langchain_tools/langchain_tools_orchestrator.py
@@ -23,9 +23,10 @@ from fastapi import HTTPException
 from langchain.agents import AgentType, initialize_agent
 from langchain.agents.agent import AgentExecutor
 from langchain.globals import set_verbose  # type: ignore
-from langchain.memory import ChatMessageHistory, ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory
 from langchain.prompts.chat import ChatPromptTemplate
 from langchain.tools import StructuredTool
+from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
 from langchain_google_vertexai import VertexAI
 from pytz import timezone
