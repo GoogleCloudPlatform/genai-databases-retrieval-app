ef39de7785e9ebebdb81f59d198aaf429839e3d5
Yuan Teoh <yuanteoh@google.com>
2024-06-24 10:35:03 -0700

    resolve comments

--- llm_demo/evaluation/evaluation.py
+++ llm_demo/evaluation/evaluation.py
@@ -41,44 +41,43 @@ class EvalData(BaseModel):
     )
 
 
-class Evaluation:
-    def load_golden_data(self) -> List[EvalData]:
-        """
-        Load golden dataset into EvalData model.
-        """
-        eval_datas = []
-        for golden in goldens:
-            for key in golden:
-                cases = golden[key]
-                for case in cases:
-                    data = EvalData(**case)
-                    data.category = key
-                    eval_datas.append(data)
-        return eval_datas
+def load_golden_data() -> List[EvalData]:
+    """
+    Load golden dataset into EvalData model.
+    """
+    eval_datas = []
+    for golden in goldens:
+        for key in golden:
+            cases = golden[key]
+            for case in cases:
+                data = EvalData(**case)
+                data.category = key
+                eval_datas.append(data)
+    return eval_datas
 
-    async def run_llm_for_eval(
-        self, eval_datas: List[EvalData], orc: BaseOrchestrator, session: Dict, uid: str
-    ) -> List[EvalData]:
-        """
-        Generate prediction_tool_calls and prediction_output for golden dataset query.
-        """
-        agent = orc.get_user_session(uid)
-        for eval_data in eval_datas:
-            query_response = await agent.invoke(eval_data.query)
+async def run_llm_for_eval(
+    eval_list: List[EvalData], orc: BaseOrchestrator, session: Dict, uid: str
+) -> List[EvalData]:
+    """
+    Generate prediction_tool_calls and prediction_output for golden dataset query.
+    """
+    agent = orc.get_user_session(uid)
+    for eval_data in eval_datas:
+        query_response = await agent.invoke(eval_data.query)
 
-            # Retrieve prediction_tool_calls from query response
-            prediction_tool_calls = []
-            for step in query_response.get("intermediate_steps"):
-                called_tool = step[0]
-                tool_call = {
-                    "name": called_tool.tool,
-                    "arguments": called_tool.tool_input,
-                }
-                prediction_tool_calls.append(tool_call)
+        # Retrieve prediction_tool_calls from query response
+        prediction_tool_calls = []
+        for step in query_response.get("intermediate_steps"):
+            called_tool = step[0]
+            tool_call = {
+                "name": called_tool.tool,
+                "arguments": called_tool.tool_input,
+            }
+            prediction_tool_calls.append(tool_call)
 
-            eval_data.prediction_tool_calls = prediction_tool_calls
-            eval_data.prediction_output = query_response.get("output")
+        eval_data.prediction_tool_calls = prediction_tool_calls
+        eval_data.prediction_output = query_response.get("output")
 
-            if eval_data.reset:
-                orc.user_session_reset(session, uid)
-        return eval_datas
+        if eval_data.reset:
+            orc.user_session_reset(session, uid)
+    return eval_datas
