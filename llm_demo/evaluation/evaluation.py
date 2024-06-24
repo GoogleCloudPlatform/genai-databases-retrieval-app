9a1e1744bc4226237e86521d3d7cbffdfc12d4d6
Yuan Teoh <yuanteoh@google.com>
2024-06-24 15:10:50 -0700

    remove irrelevant functions

--- llm_demo/evaluation/evaluation.py
+++ llm_demo/evaluation/evaluation.py
@@ -12,57 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
-from typing import Any, Dict, List, Optional
-
-from pydantic import BaseModel, Field
+from typing import Dict, List
 
 from orchestrator import BaseOrchestrator
 
-from .eval_golden import goldens
+from .eval_golden import EvalData
 
-
-class EvalData(BaseModel):
-    category: Optional[str] = Field(default=None, description="evaluation category")
-    query: Optional[str] = Field(default=None, description="user query")
-    instruction: Optional[str] = Field(
-        default=None, description="instruction to llm system"
-    )
-    content: Optional[str] = Field(default=None)
-    tool_calls: Optional[List[Dict[str, Any]]] = Field(default=None)
-    context: Optional[str] = Field(
-        default=None, description="context given to llm in order to answer user query"
-    )
-    output: Optional[str] = Field(default=None)
-    prediction_tool_calls: Optional[List[Dict[str, Any]]] = Field(default=None)
-    prediction_output: Optional[str] = Field(default=None)
-    reset: bool = Field(
-        default=True, description="determine to reset the chat after invoke"
-    )
-
-
-def load_golden_data() -> List[EvalData]:
-    """
-    Load golden dataset into EvalData model.
-    """
-    eval_datas = []
-    for golden in goldens:
-        for key in golden:
-            cases = golden[key]
-            for case in cases:
-                data = EvalData(**case)
-                data.category = key
-                eval_datas.append(data)
-    return eval_datas
 
 async def run_llm_for_eval(
-    eval_list: List[EvalData], orc: BaseOrchestrator, session: Dict, uid: str
+    eval_list: List[EvalData], orc: BaseOrchestrator, session: Dict, session_id: str
 ) -> List[EvalData]:
     """
     Generate prediction_tool_calls and prediction_output for golden dataset query.
     """
-    agent = orc.get_user_session(uid)
-    for eval_data in eval_datas:
+    agent = orc.get_user_session(session_id)
+    for eval_data in eval_list:
         query_response = await agent.invoke(eval_data.query)
 
         # Retrieve prediction_tool_calls from query response
@@ -79,5 +43,5 @@ async def run_llm_for_eval(
         eval_data.prediction_output = query_response.get("output")
 
         if eval_data.reset:
-            orc.user_session_reset(session, uid)
-    return eval_datas
+            orc.user_session_reset(session, session_id)
+    return eval_list
