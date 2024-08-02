20cadb9c0e55f7914d829711ebd8a9918b827057
fernandadeleon <fernanda.-deleon@hotmail.com>
2024-08-01 21:27:15 -0400

    Change driver for data_set

--- retrieval_service/datastore/providers/neo4jdb_test.py
+++ retrieval_service/datastore/providers/neo4jdb_test.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Google LLC
+# Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,23 +12,18 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import asyncio
 from typing import AsyncGenerator
 
 import pytest
 import pytest_asyncio
-from neo4j import AsyncGraphDatabase
+
+import models
 
 from .. import datastore
 from . import neo4jdb
 from .utils import get_env_var
 
-
-@pytest.fixture(scope="session")
-def event_loop():
-    loop = asyncio.get_event_loop()
-    yield loop
-    loop.close()
+pytestmark = pytest.mark.asyncio(scope="module")
 
 
 @pytest.fixture(scope="module")
@@ -52,13 +47,6 @@ def db_auth(db_user, db_password) -> tuple:
 
 
 @pytest_asyncio.fixture(scope="module")
-async def driver(db_uri: str, db_auth: tuple) -> AsyncGenerator:
-    driver = AsyncGraphDatabase.driver(db_uri, auth=db_auth)
-    yield driver
-    await driver.close()
-
-
-@pytest_asyncio.fixture(scope="module")
 async def data_set(
     db_uri: str,
     db_auth: tuple,
@@ -76,7 +64,7 @@ async def data_set(
     flights_data_set_path = "../data/flights_dataset.csv"
     policies_data_set_path = "../data/cymbalair_policy.csv"
 
-    airport, amenities, flights, polcies = await client.load_dataset(
+    airport, amenities, flights, policies = await client.load_dataset(
         airports_data_set_path,
         amenities_data_set_path,
         flights_data_set_path,
@@ -91,38 +79,32 @@ async def data_set(
     await client.close()
 
 
-@pytest.mark.asyncio
-async def test_amenity_init(driver):
-    async with driver.session() as session:
-        result = await session.run("MATCH (a: Amenity) RETURN count(a) AS count")
-        record = await result.single()
-        count = record["count"]
-        print(count)
+async def test_amenity_init_id(data_set: neo4jdb.Client):
+    amenity = await data_set.get_amenity(35)
 
-    expected_count = 127
-    assert count == expected_count
+    expected_amenity = models.Amenity(
+        id=35,
+        name="Airport Information Desk",
+        description="Information desk offering assistance with flight information, directions, and other airport services.",
+        location="Arrivals Hall",
+        terminal="All Terminals",
+        category="facility",
+        hour="24/7",
+        sunday_start_hour=None,
+        sunday_end_hour=None,
+        monday_start_hour=None,
+        monday_end_hour=None,
+        tuesday_start_hour=None,
+        tuesday_end_hour=None,
+        wednesday_start_hour=None,
+        wednesday_end_hour=None,
+        thursday_start_hour=None,
+        thursday_end_hour=None,
+        friday_start_hour=None,
+        friday_end_hour=None,
+        saturday_start_hour=None,
+        saturday_end_hour=None,
+        embedding=None,
+    )
 
-
-@pytest.mark.asyncio
-async def test_amenity_init_id(driver):
-    async with driver.session() as session:
-        amenity_id = 35
-        result = await session.run(
-            "MATCH (a: Amenity {id: $id}) RETURN a", id=amenity_id
-        )
-        record = await result.single()
-        node = dict(record["a"])
-
-        if not record:
-            raise AssertionError(f"No amenity found with id {amenity_id}")
-
-    expected_node = {
-        "id": 35,
-        "name": "Airport Information Desk",
-        "description": "Information desk offering assistance with flight information, directions, and other airport services.",
-        "location": "Arrivals Hall",
-        "terminal": "All Terminals",
-        "category": "facility",
-        "hour": "24/7",
-    }
-    assert node == expected_node
+    assert amenity == expected_amenity
