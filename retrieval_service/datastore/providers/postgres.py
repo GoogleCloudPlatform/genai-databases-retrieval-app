2907d4aea991b951500e4a660e907382203180c1
Anubhav Dhawan <anubhav756@gmail.com>
2024-09-21 02:26:20 +0530

    fix: Remove unused modules

--- retrieval_service/datastore/providers/postgres.py
+++ retrieval_service/datastore/providers/postgres.py
@@ -12,8 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import asyncio
-from datetime import datetime
 from ipaddress import IPv4Address, IPv6Address
 from typing import Any, Literal, Optional
 
