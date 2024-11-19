676229c87f54601f3c4d0492ac99360b0dc0ebc4
Anubhav Dhawan <anubhav756@gmail.com>
2024-11-19 22:42:29 +0530

    dep: Upgrade LangChain version and other modules. (#509)
    * Pin Retrieval Service and LLM Demo modules to latest versions.
    * Upgrade test module requirements for LLM Demo and Retrieval Service.
    * Fix google-cloud-alloydb-connector version.
    * Use asyncio dependencies in sqlalchemy.
    
    ---------
    
    Co-authored-by: Yuan <45984206+Yuan325@users.noreply.github.com>

--- llm_demo/orchestrator/langchain_tools/langchain_tools_orchestrator.py
+++ llm_demo/orchestrator/langchain_tools/langchain_tools_orchestrator.py
@@ -24,10 +24,10 @@ from langchain.agents import AgentType, initialize_agent
 from langchain.agents.agent import AgentExecutor
 from langchain.globals import set_verbose  # type: ignore
 from langchain.memory import ConversationBufferMemory
-from langchain.prompts.chat import ChatPromptTemplate
-from langchain.tools import StructuredTool
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.tools import StructuredTool
 from langchain_google_vertexai import VertexAI
 from pytz import timezone
 
