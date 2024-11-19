903ed839d87ad0810cc5e572dfd04306cb64aa30
Yuan <45984206+Yuan325@users.noreply.github.com>
2024-11-18 19:03:06 -0800

    Update langchain_tools_orchestrator.py

--- llm_demo/orchestrator/langchain_tools/langchain_tools_orchestrator.py
+++ llm_demo/orchestrator/langchain_tools/langchain_tools_orchestrator.py
@@ -24,7 +24,7 @@ from langchain.agents import AgentType, initialize_agent
 from langchain.agents.agent import AgentExecutor
 from langchain.globals import set_verbose  # type: ignore
 from langchain.memory import ConversationBufferMemory
-from langchain.prompts.chat import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.messages import AIMessage, BaseMessage, HumanMessage
 from langchain_core.tools import StructuredTool
