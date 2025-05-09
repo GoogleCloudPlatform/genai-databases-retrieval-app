c1c252252a378328c9142179b56f8f95efc7f228
Tom Bottrill <tombot@google.com>
2025-05-09 08:15:44 +0000

    Fixes app init

--- retrieval_service/app/app.py
+++ retrieval_service/app/app.py
@@ -35,7 +35,7 @@ class AppConfig(BaseModel):
     clientId: Optional[str] = None
 
 
-def parse_config(path: str) -> AppConfig:
+def parse_config() -> AppConfig:
     config = {}
 
     # Base config
