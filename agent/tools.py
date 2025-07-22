c80dd97620bd07cc58c53e23f1df2365df3f9b11
Anubhav Dhawan <anubhavdhawan@google.com>
2025-07-22 20:48:49 +0530

    fix: Update toolbox client authentication to use the newer method.

--- agent/tools.py
+++ agent/tools.py
@@ -13,44 +13,20 @@
 # limitations under the License.
 
 import os
-from typing import Optional
 
-from google.auth.exceptions import DefaultCredentialsError
-from google.auth.transport.requests import Request
-from google.oauth2.id_token import fetch_id_token
+from toolbox_core import auth_methods
 from toolbox_langchain import ToolboxClient
 
 BASE_URL = os.getenv("BASE_URL", default="http://127.0.0.1:8080")
 TOOLBOX_URL = os.getenv("TOOLBOX_URL", default="http://127.0.0.1:5000")
 
 
-def __get_client_headers() -> Optional[dict[str, str]]:
-    """
-    Fetches a Google Cloud identity token for authenticating with the Toolbox
-    service.
-
-    This function uses the application's default credentials to generate an
-    identity token.
-
-    Returns:
-        Optional[dict[str, str]]: A dictionary containing the Authorization
-        header if authentication is successful, otherwise None.
-    """
-    try:
-        id_token = fetch_id_token(Request(), TOOLBOX_URL)
-        return {"Authorization": f"Bearer {id_token}"}
-    except DefaultCredentialsError:
-        # Inform the user that token generation failed, which is expected
-        # for local runs, and continue without authentication.
-        print(
-            "GOOGLE_APPLICATION_CREDENTIALS env var is not set. Proceeding without setting client authentication header."
-        )
-        return None
-
-
 # Tools for agent
 async def initialize_tools():
-    client = ToolboxClient(TOOLBOX_URL, client_headers=__get_client_headers())
+    auth_token_provider = auth_methods.aget_google_id_token(TOOLBOX_URL)
+    client = ToolboxClient(
+        TOOLBOX_URL, client_headers={"Authorization": auth_token_provider}
+    )
     tools = await client.aload_toolset("cymbal_air")
 
     # Load insert_ticket and validate_ticket tools separately to implement
